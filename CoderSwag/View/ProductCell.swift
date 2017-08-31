4d6e54d56300050ea73a172c4b69bd637de0885c
Mark Rabins <markrabins@gmail.com>
2017-08-31 11:45:02 -0700

    Set Up for Product Collection View

---
+++ CoderSwag/View/ProductCell.swift
@@ -0,0 +1,18 @@
+//
+//  ProductCell.swift
+//  CoderSwag
+//
+//  Created by Mark Rabins on 8/31/17.
+//  Copyright © 2017 self. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+    
+}
